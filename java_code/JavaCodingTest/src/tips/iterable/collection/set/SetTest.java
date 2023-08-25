7c00b8fce007da16a96d62768a44010bc66743c2
5innim <bjm22247@gmail.com>
2023-08-25 11:17:59 +0900

    Add tip

--- java_code/JavaCodingTest/src/tips/iterable/collection/set/SetTest.java
+++ java_code/JavaCodingTest/src/tips/iterable/collection/set/SetTest.java
@@ -1,7 +1,9 @@
-package tips.collections;
+package tips.iterable.collection.set;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SetTest {
     public static void main(String[] args){
@@ -21,6 +23,13 @@ public class SetTest {
         // 제네릭스 이용: 저장되는 데이터 타입 제한
         Set<String> set1 = new HashSet<>();
         set1.add("element1");
+        set1.add("element2");
+
+        // Stream 이용하기
+        Stream<String> stream = set1.stream();
+        System.out.println(stream.collect(Collectors.toList())); // stream 에 대해 종료작업을 하였기에 아래는 실행시 에러발생.
+        //System.out.println(stream.sorted().collect(Collectors.toList()));
+
 
 
     }
