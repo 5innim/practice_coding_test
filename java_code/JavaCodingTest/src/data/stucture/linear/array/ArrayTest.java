459e05fc879c9d5c804bb184f11ef287b56d067c
5innim <bjm22247@gmail.com>
2023-08-25 11:21:06 +0900

    Add algorithm

---
+++ java_code/JavaCodingTest/src/data/stucture/linear/array/ArrayTest.java
@@ -0,0 +1,17 @@
+package data.stucture.linear.array;
+
+public class ArrayTest {
+
+    /**
+     * 배열
+     *  - 크기가 정해져있다.
+     *  - 인덱스를 사용해 요소에 빠른접근 가능
+     *  - 배열의 크기는 고정되어 있기 때문에 삽입 및 삭제가 실질적으로 불가능
+     *
+     */
+    public static void main (String[] args) {
+
+
+    }
+
+}
