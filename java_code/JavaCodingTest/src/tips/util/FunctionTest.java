f0ac66248142ff52854f2d115bda77083e6b8249
5innim <bjm22247@gmail.com>
2023-08-25 11:20:50 +0900

    Add tip

---
+++ java_code/JavaCodingTest/src/tips/util/FunctionTest.java
@@ -0,0 +1,19 @@
+package tips.util;
+
+public class FunctionTest {
+
+    /**
+     * Predicate	           T -> boolean	             boolean test(T t)
+     * Consumer	               T -> void	             void accept(T t)
+     * Supplier	              () -> T	                 T get()
+     * Function<T, R>	       T -> R	                 R apply(T t)
+     * Comparator	      (T, T) -> int	                 int compare(T o1, T o2)
+     * Runnable	              () -> void	             void run()
+     * Callable	              () -> T	                 V call()
+     */
+
+    public void main(String[] args) {
+
+    }
+
+}
