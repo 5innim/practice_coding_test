2bd67a2be84a8c9a1072d23eafebaef8c0928338
5innim <bjm22247@gmail.com>
2024-03-03 14:56:13 +0900

    Add info for HashSet

--- java_code/JavaCodingTest/src/data/stucture/nonlinear/SetTest.java
+++ java_code/JavaCodingTest/src/data/stucture/nonlinear/SetTest.java
@@ -9,6 +9,8 @@ public class SetTest {
      * set: 중복을 허용하지 않는 자료구조, 순서가 없음.
      *   - 조회, 삽입, 삭제 모두 O(1)
      *   - 내부적으로 Object의 hashCode() 함수를 이용하여 객체의 일치여부를 확인한다.
+     *   - HashSet 은 내부적으로 HashMap 으로 구현되어 있다. hashcode 를 key 로 하는 bucket 을 만들기 때문에
+     *     contains() 의 시간복잡도가 O(1) 이 되는 것이다.
      */
 
     public static void main(String[] args) {
